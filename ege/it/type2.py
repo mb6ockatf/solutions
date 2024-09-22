6ad64a75f94e93b8e46c90e2b6518bbbf2c7b2ba
mb6ockatf <mdddmmmm@yandex.ru>
2024-09-22 18:47:30 +0300

    feat(ege.it): solve tasks

--- ege/it/type2.py
+++ ege/it/type2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from itertools import product, permutations
 
 
 def convert(data: list) -> list:
@@ -14,13 +15,9 @@ def f_64932(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_64932():
     print("z y x w")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    if not f_64932(w, x, y, z):
-                        continue
-                    print(*convert([z, y, x, w]))
+    for w, x, y, z in product([True, False], repeat=4):
+        if f_64932(w, x, y, z):
+            print(*convert([z, y, x, w]))
 
 
 # -----------------------------------------------------------------------------
@@ -30,13 +27,10 @@ def f_68503(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_68503():
     print("z x y w")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    if f_68503(w, x, y, z):
-                        continue
-                    print(*convert([z, x, y, w]))
+    for w, x, y, z in product([True, False], repeat=4):
+        if f_68503(w, x, y, z):
+            continue
+        print(*convert([z, x, y, w]))
 
 
 # -----------------------------------------------------------------------------
@@ -46,13 +40,9 @@ def f_38534(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_38534():
     print("w x y z")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    if not f_38534(w, x, y, z):
-                        continue
-                    print(*convert([w, x, y, z]))
+    for w, x, y, z in product([True, False], repeat=4):
+        if f_38534(w, x, y, z):
+            print(*convert([w, x, y, z]))
 
 
 # -----------------------------------------------------------------------------
@@ -63,16 +53,13 @@ def f_18483(w: bool, x: bool, y: bool, z: bool) -> bool:
 def task_18483():
     lines = []
     print("y z w x F")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    f = f_18483(w, x, y, z)
-                    container = [y, z, w, x, f]
-                    #                   if (not f) and (container.count(True) == 3):
-                    #                       print("SELECTED:", *container)
-                    if container not in lines and f:
-                        lines.append(container)
+    for w, x, y, z in product([True, False], repeat=4):
+        f = f_18483(w, x, y, z)
+        container = [y, z, w, x, f]
+#                   if (not f) and (container.count(True) == 3):
+#                       print("SELECTED:", *container)
+        if container not in lines and f:
+            lines.append(container)
     for container in lines:
         print(*convert(container))
 
@@ -84,13 +71,9 @@ def f_28677(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_28677():
     print("z y x w")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    if not f_28677(w, x, y, z):
-                        continue
-                    print(*convert([z, y, x, w]))
+    for w, x, y, z in product([True, False], repeat=4):
+        if f_28677(w, x, y, z):
+            print(*convert([z, y, x, w]))
 
 
 # -----------------------------------------------------------------------------
@@ -100,13 +83,9 @@ def f_18430(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_18430():
     print("w z y x")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    if f_18430(w, x, y, z):
-                        continue
-                    print(*convert([w, z, y, x]))
+    for w, x, y, z in product([True, False], repeat=4):
+        if not f_18430(w, x, y, z):
+            print(*convert([w, z, y, x]))
 
 
 # -----------------------------------------------------------------------------
@@ -117,15 +96,12 @@ def f_18550(w: bool, x: bool, y: bool, z: bool) -> bool:
 def task_18550():
     lines = []
     print("z w y x F")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    f = f_18550(w, x, y, z)
-                    container = [z, w, y, x, f]
-                    if not f or container in lines:
-                        continue
-                    lines.append(container)
+    for w, x, y, z in product([True, False], repeat=4):
+        f = f_18550(w, x, y, z)
+        container = [z, w, y, x, f]
+        if f or container in lines:
+            continue
+        lines.append(container)
     for container in lines:
         print(*convert(container))
 
@@ -138,15 +114,12 @@ def f_27260(w: bool, x: bool, y: bool, z: bool) -> bool:
 def task_27260():
     print("y z w x")
     lines = []
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    f = f_27260(w, x, y, z)
-                    container = [y, z, w, x]
-                    if f or container in lines:
-                        continue
-                    lines.append(container)
+    for w, x, y, z in product([True, False], repeat=4):
+        f = f_27260(w, x, y, z)
+        container = [y, z, w, x]
+        if f or container in lines:
+            continue
+        lines.append(container)
     for container in lines:
         print(*convert(container))
 
@@ -158,14 +131,11 @@ def f_29187(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_29187():
     print("x z w y")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    f = f_29187(w, x, y, z)
-                    if not f:
-                        continue
-                    print(*convert([x, z, w, y]))
+    for w, x, y, z in product([True, False], repeat=4):
+        f = f_29187(w, x, y, z)
+        if not f:
+            continue
+        print(*convert([x, z, w, y]))
 
 
 # -----------------------------------------------------------------------------
@@ -175,14 +145,10 @@ def f_16878(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_16878():
     print("z w y x")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    f = f_16878(w, x, y, z)
-                    if f:
-                        continue
-                    print(*convert([z, w, y, x]))
+    for w, x, y, z in product([True, False], repeat=4):
+        f = f_16878(w, x, y, z)
+        if not f:
+            print(*convert([z, w, y, x]))
 
 
 # -----------------------------------------------------------------------------
@@ -192,15 +158,123 @@ def f_27371(w: bool, x: bool, y: bool, z: bool) -> bool:
 
 def task_27371():
     print("z y w x")
-    for w in (False, True):
-        for x in (False, True):
-            for y in (False, True):
-                for z in (False, True):
-                    f = f_27371(w, x, y, z)
-                    if f:
-                        continue
-                    print(*convert([z, y, w, x]))
+    for w, x, y, z in product([True, False], repeat=4):
+        f = f_27371(w, x, y, z)
+        if not f:
+            print(*convert([z, y, w, x]))
 
+
+# -----------------------------------------------------------------------------
+def f_37137(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return (not w and not x) or (x == y) or z
+
+def task_37137():
+    print("y z x w")
+    for w, x, y, z in product([True, False], repeat=4):
+        if not f_37137(w, x, y, z):
+            print(*convert([y, z, x, w]))
+
+# -----------------------------------------------------------------------------
+
+def f_15912(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return ((x <= y) == (z <= w)) or (x and w)
+
+def task_15912():
+    print("z y x w")
+    data = []
+    for w, x, y, z in product([True, False], repeat=4):
+        if not f_15912(w, x, y, z):
+            print(*convert([z, y, x, w]))
+
+# -----------------------------------------------------------------------------
+
+def f_18781(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return (not x or not y) and not (x == z) and w
+
+def task_18781():
+    print("w x y z")
+    for w, x, y, z in product([True, False], repeat=4):
+        if f_18781(w, x, y, z):
+            print(*convert([w, x, y, z]))
+
+# -----------------------------------------------------------------------------
+
+
+def f_68274(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return ((y or z) <= (z and w)) == (not ((x and z)<=(w or y)))
+
+def task_68274():
+    print("w y x z")
+    for w, x, y, z in product([True, False], repeat=4):
+        if f_68274(w, x, y, z) == 1:
+            print(*convert([w, y, x, z]))
+
+# -----------------------------------------------------------------------------
+
+def f_18071(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return (x and not y) or (y == z) or not w
+
+def task_18071():
+    print("x w z y")
+    for w, x, y, z in product([True, False], repeat=4):
+        if not f_18071(w, x, y, z):
+            print(*convert([x, w, z, y]))
+
+# -----------------------------------------------------------------------------
+
+def f_48450(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return (w <= (y == z)) and (y == (z <= x))
+
+def task_48450():
+    print("z w y x f")
+    for w, x, y, z in product([True, False], repeat=4):
+        f = f_48450(w, x, y, z)
+        if not f and [w, x, y, z] != [True, True, True, True]:
+            print(*convert([z, w, y, x, f]))
+
+# -----------------------------------------------------------------------------
+
+def f_14763(x: bool, y: bool, z: bool) -> bool:
+    return (x or y) <= (y == z)
+
+def task_14763():
+    print("z x y")
+    for x, y, z in product([True, False], repeat=3):
+        if not f_14763(x, y, z):
+            print(*convert([z, x, y]))
+
+# -----------------------------------------------------------------------------
+
+def f_70529(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return ((w <= y) <= x) or not z
+
+def task_70529():
+    print("z y w x")
+    for w, x, y, z in product([True, False], repeat=4):
+        if not f_70529(w, x, y, z):
+            print(*convert([z, y, w, x]))
+
+# -----------------------------------------------------------------------------
+
+def f_39231(w: bool, x: bool, y: bool, z: bool) -> bool:
+    return (not z == y) <= ((w and not x) == (y and x))
+
+def task_39231():
+    print("z w x y")
+    for w, x, y, z in product([True, False], repeat=4):
+        if not f_39231(w, x, y, z):
+            print(*convert([z, w, x, y]))
+
+# -----------------------------------------------------------------------------
+
+def f_63018(u: bool, w: bool, x: bool, y: bool, z: bool) -> bool:
+    return ((x <= y) and (z == (not w))) <= (u == (x or z))
+
+def task_63018():
+    print("w z y x u")
+    for u, w, x, y, z in product([True, False], repeat=5):
+        if not f_63018(u, w, x, y, z):
+            print(*convert([w, z, y, x, u]))
 
 if __name__ == "__main__":
-    task_27371()
+    task_63018()
--- ege/it/type6.py
+++ ege/it/type6.py
@@ -150,6 +150,107 @@ def task_59684():
     input()
     reset()
 
+def task_47308():
+    for _ in range(5):
+        forward(8 * k)
+        right(60)
+        forward(8 * k)
+        right(120)
+    up()
+    for x in range(-20, 20):
+        for y in range(0, 20):
+            goto(x * k, y * k)
+            dot(4)
+    input()
+    reset()
+
+def task_47310():
+    for _ in range(4):
+        forward(6 * k)
+        right(150)
+        forward(6 * k)
+        right(30)
+    up()
+    for x in range(-5, 10):
+        for y in range(-5, 10):
+            goto(x * k, y * k)
+            dot(4)
+    input()
+    reset()
+
+
+def task_47248():
+    for _ in range(4):
+        forward(10 * k)
+        right(90)
+    up()
+    for x in range(-4, 16):
+        for y in range(-4, 16):
+            goto(x * k, y * k)
+            dot(4)
+    input()
+    reset()
+
+def task_47394():
+    for _ in range(10):
+        forward(k * 5)
+        right(60)
+    up()
+    for x in range(-4, 10):
+        for y in range(-4, 15):
+            goto(x * k, y * k)
+            dot(4)
+    input()
+    reset()
+
+
+def task_51975():
+    for _ in range(4):
+        forward(k * 7)
+        right(90)
+        forward(7 * k)
+        left(90)
+        forward(7 * k)
+        right(90)
+    up()
+    for x in range(10):
+        for y in range(-10, 16):
+            goto(x * k, y * k)
+            dot(4)
+    input()
+    reset()
+
+def task_47315():
+    for _ in range(6):
+        forward(7 * k)
+        right(90)
+        forward(7 * k)
+        right(90)
+    up()
+    for x in range(10):
+        for y in range(10):
+            goto(x * k, y * k)
+            dot(4)
+    input()
+    reset()
+
+def task_47406():
+    color("red")
+    for _ in range(4):
+        forward(12 * k)
+        right(90)
+    color("blue")
+    for _ in range(3):
+        forward(12 * k)
+        right(120)
+    up()
+    for x in range(15):
+        for y in range(15):
+            goto(x * k, y * k)
+            dot(4)
+    input()
+    reset()
+
 
 if __name__ == "__main__":
-    task_59684()
+    task_47406()
