0ec61cfe6367fa997cbb1a01cf30b9cc630daab5
mb6ockatf <mdddmmmm@yandex.ru>
2025-01-18 20:58:26 +0300

    feat(ege.it): complete more tasks

--- ege/it/type5.py
+++ ege/it/type5.py
@@ -138,6 +138,72 @@ def task_69883() -> int:
         if int(data, 2) < 35:
             return i
 
+def task_15974() -> int:
+    maximal_result = 0
+    for N in range(1, 10**10):
+        N_bin = bin(N)[2:]
+        if N % 2 == 0:
+            N_bin += "10"
+        else:
+            N_bin += "01"
+        R = int(N_bin, 2)
+        if R <= 102 and R > maximal_result:
+            maximal_result = R
+        elif R > 200:
+            break
+    return maximal_result
+
+def task_16381() -> int:
+    for N in range(1, 10**10):
+        N_bin = bin(N)[2:-1]
+        if N % 2 == 0:
+            N_bin += "01"
+        else:
+            N_bin += "10"
+        R = int(N_bin, 2)
+        if R == 2018:
+            return N
+
+def task_68238() -> int:
+    for N in range(100, 10**10):
+        N_str = str(N)
+        data = set()
+        for index in range(len(N_str) - 2):
+            current_string = N_str[index:index + 3]
+            data.add(int(current_string))
+        maximal, minimal = max(data), min(data)
+        R = maximal - minimal
+        if R == 415:
+            return N
+
+
+def task_33084() -> int:
+    for N in range(1, 10**10):
+        N_bin = bin(N)[2:]
+        N_bin += str(N_bin.count("1") % 2)
+        N_bin += str(N_bin.count("1") % 2)
+        R = int(N_bin, 2)
+        if R > 154:
+            return N
+
+def task_68267() -> int:
+    for N in range(100, 10**10):
+        N_str = str(N)
+        data = set()
+        for index in range(len(N_str) - 2):
+            current_string = N_str[index:index + 3]
+            data.add(int(current_string))
+        maximal, minimal = max(data), min(data)
+        R = maximal - minimal
+        if R == 623:
+            return N
+
+def task_55622() -> int:
+    a = 876_544
+    b = 1_234_567_899
+    return (b - a) // 8 + 1
+
+def task_16435
 
 if __name__ == "__main__":
-    print(task_69883())
+    print(task_55622())
