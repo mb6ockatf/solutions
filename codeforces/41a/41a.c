b4cfc920c9b33f9754bb9c2cbe272dfe7e8c967d
mb6ockatf <mdddmmmm@yandex.ru>
2025-02-12 00:26:00 +0300

    feat(41a): added

---
+++ codeforces/41a/41a.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char s[1000];
+	scanf("%[^\n]s", s);
+	unsigned short length = 0, i = 0;
+	length = (unsigned short int)strlen(s);
+	char t[1000];
+	scanf("%s", t);
+	if (strlen(s) != strlen(t))
+		{
+			printf("NO\n");
+			return 0;
+		}
+	for (i = 0; i < length; i++)
+		{
+			if (s[i] != t[length - i - 1])
+				{
+					printf("NO\n");
+					return 0;
+				}
+		}
+	printf("YES\n");
+	return 0;
+}
