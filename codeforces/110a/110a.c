56261486740c1b67311e14a15d49d6bda8bcb188
mb6ockatf <mdddmmmm@yandex.ru>
2025-02-12 00:22:53 +0300

    feat(110a): added

---
+++ codeforces/110a/110a.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(void)
+{
+	long long n, r = 0, c = 0;
+	scanf("%lld", &n);
+	while (n > 0)
+		{
+			r = n % 10;
+			if (r == 4 || r == 7) c++;
+			n /= 10;
+		}
+	if (c == 0)
+		{
+			printf("NO\n");
+			return 0;
+		}
+	while (c > 0)
+		{
+			r = c % 10;
+			if (r != 4 && r != 7)
+				{
+					printf("NO\n");
+					return 0;
+				}
+			c /= 10;
+		}
+	printf("YES\n");
+	return 0;
+}
